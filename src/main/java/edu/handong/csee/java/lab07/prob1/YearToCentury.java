13ee03d916654121a45e38fabec43fdfdd41360b
bbibbd <bbibochaa74@gmail.com>
2018-03-30 22:44:34 +0900

    constructor

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -1,7 +1,13 @@
 package edu.handong.csee.java.lab07.prob1;
+import java.util.Scanner;
 
 public class YearToCentury {
 
+	int year;
+	public void IsCentury() {
+		int century;
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
