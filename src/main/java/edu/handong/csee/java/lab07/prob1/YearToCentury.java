212ef017387a38e00fa2b3a11d5041fed4165c19
bbibbd <bbibochaa74@gmail.com>
2018-04-02 19:14:07 +0900

    Before Comments

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -1,16 +1,33 @@
 package edu.handong.csee.java.lab07.prob1;
 import java.util.Scanner;
 
-public class YearToCentury {
-
+public class YearToCentury{
 	int year;
-	public void IsCentury(int century) {
+	
+	public YearToCentury() {
+		year=0;
+	}
+	
+	public YearToCentury(int year) {
+		this.year=year;
+	}
+	
+	public int calc_century() {
+		int century;
+		if(year%100==0)	century = year/100;
+		else century = year/100+1;
+		return century;
+	}
+	
+	public static void main(String [] args) {
+		int yr = 0;
+		Scanner keyboard = new Scanner(System.in);
 		
+		System.out.print("Input year: ");
+		yr = keyboard.nextInt();
+
+		YearToCentury year = new YearToCentury(yr);
+		System.out.println(yr+ "is "+ year.calc_century()+"th century");
 		
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
