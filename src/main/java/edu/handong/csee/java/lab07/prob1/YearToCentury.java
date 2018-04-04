7ac9ebf8119533736b0d1be9ac8ca216073aa19f
bbibbd <bbibochaa74@gmail.com>
2018-04-04 10:08:12 +0900

    YearToCentury - before javadoc

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -1,33 +1,38 @@
-package edu.handong.csee.java.lab07.prob1;
-import java.util.Scanner;
+package edu.handong.csee.java.lab07.prob1;	// name of the package and the directory where the class file will be saved
+import java.util.Scanner;	// Import the class 'Scanner'
 
-public class YearToCentury{
-	int year;
+/**
+ * This class will get the year from user and calculate the century
+ * @author KKB
+ *
+ */
+public class YearToCentury{		//Declare the class for public so that any package and class can call this class.
+	int year;	//declare class variables so that any method in the class can use this two variables.
 	
-	public YearToCentury() {
-		year=0;
+	/**
+	 * This constructor will put user inputed value to the class value 'year'
+	 * @param year
+	 */
+	public YearToCentury(int year) {	// declare the constructor
+		this.year=year;	//get user inputed value and put the class value 'year'
 	}
 	
-	public YearToCentury(int year) {
-		this.year=year;
+	public int calc_century() {	//declare the method
+		int century;	//declare the local variable 'century'
+		if(year%100==0)	century = year/100;	//if year divided by 100 has no remains, century is year divided by 100
+		else century = year/100+1;	//if it has remains, century is year divided by 100 plus 1
+		return century;	//return the century
 	}
 	
-	public int calc_century() {
-		int century;
-		if(year%100==0)	century = year/100;
-		else century = year/100+1;
-		return century;
-	}
-	
-	public static void main(String [] args) {
-		int yr = 0;
-		Scanner keyboard = new Scanner(System.in);
+	public static void main(String [] args) {	//declare the main method
+		int yr = 0;	//initiate the variable 'yr' to 0
+		Scanner keyboard = new Scanner(System.in);	//instantiate the Scanner to keyboard
 		
-		System.out.print("Input year: ");
-		yr = keyboard.nextInt();
+		System.out.print("Input year: ");	//print out this massage to get the year from user
+		yr = keyboard.nextInt();	//put user inputed value to the variable 'yr'
 
-		YearToCentury year = new YearToCentury(yr);
-		System.out.println(yr+ "is "+ year.calc_century()+"th century");
+		YearToCentury year = new YearToCentury(yr);	//instantiate and initiate the class and the class value to year using constructor
+		System.out.println(yr+ "is "+ year.calc_century()+"th century");	//print out the result of the calculation. 
 		
 	}
 }
