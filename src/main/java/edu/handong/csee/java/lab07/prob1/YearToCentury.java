4d8ab528bf675419ef4183ffc6f8b40763c82730
bbibbd <bbibochaa74@gmail.com>
2018-04-04 10:11:30 +0900

    YearToCentury - after javadoc

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -2,7 +2,7 @@ package edu.handong.csee.java.lab07.prob1;	// name of the package and the direct
 import java.util.Scanner;	// Import the class 'Scanner'
 
 /**
- * This class will get the year from user and calculate the century
+ * This class will get the year from user and calculate the century.
  * @author KKB
  *
  */
@@ -10,13 +10,17 @@ public class YearToCentury{		//Declare the class for public so that any package 
 	int year;	//declare class variables so that any method in the class can use this two variables.
 	
 	/**
-	 * This constructor will put user inputed value to the class value 'year'
+	 * This constructor will put user inputed value to the class value 'year'.
 	 * @param year
 	 */
 	public YearToCentury(int year) {	// declare the constructor
 		this.year=year;	//get user inputed value and put the class value 'year'
 	}
 	
+	/**
+	 * This method converts the year user inputed to century. 
+	 * @return
+	 */
 	public int calc_century() {	//declare the method
 		int century;	//declare the local variable 'century'
 		if(year%100==0)	century = year/100;	//if year divided by 100 has no remains, century is year divided by 100
@@ -24,6 +28,10 @@ public class YearToCentury{		//Declare the class for public so that any package 
 		return century;	//return the century
 	}
 	
+	/**
+	 * This main method gets year from user and print out the result of converting year to century
+	 * @param args
+	 */
 	public static void main(String [] args) {	//declare the main method
 		int yr = 0;	//initiate the variable 'yr' to 0
 		Scanner keyboard = new Scanner(System.in);	//instantiate the Scanner to keyboard
